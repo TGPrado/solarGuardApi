9d09e73493e71c975775984a7a4c386e36d945c8
Thiago Prado <prado_thiago@hotmail.com>
2024-08-27 22:37:57 -0300

    feat: documentation

--- internal/app/app.go
+++ internal/app/app.go
@@ -6,12 +6,15 @@ import (
 	"syscall"
 
 	"github.com/TGPrado/GoScaffoldApi/config"
+	_ "github.com/TGPrado/GoScaffoldApi/docs"
 	v1 "github.com/TGPrado/GoScaffoldApi/internal/routers/v1"
 	"github.com/TGPrado/GoScaffoldApi/pkg/database"
 	"github.com/TGPrado/GoScaffoldApi/pkg/logger"
 	httpServer "github.com/TGPrado/GoScaffoldApi/pkg/server"
 	"github.com/gin-gonic/gin"
 	"github.com/rs/zerolog"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
 
 func Run(cfg *config.Config) {
@@ -23,7 +26,9 @@ func Run(cfg *config.Config) {
 	}
 
 	handler := gin.Default()
+	handler.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 	v1.NewRouter(handler, l, db)
+
 	httpServer := httpServer.New(handler, httpServer.Port(cfg.HTTP.Port))
 
 	if err := gracefulShutdown(httpServer, l); err != nil {
--- internal/controllers/v1/health.go
+++ internal/controllers/v1/health.go
@@ -8,10 +8,20 @@ import (
 
 type HealthController struct{}
 
+type HealthCheckResponse struct {
+	Message string `json:"message"`
+}
+
 func NewHealthController() *HealthController {
 	return &HealthController{}
 }
 
+// @Summary Health Check
+// @Description Retorna o status de saúde da aplicação
+// @Tags Health
+// @Produce  json
+// @Success 200 {object} HealthCheckResponse
+// @Router /health [get]
 func (h *HealthController) HealthCheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"message": "Ok"})
 }
